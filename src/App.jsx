4908a1fdd320eb9ac3c0341e8015cbd26668f587
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-05-08 10:33:40 -0300

    Ordenação Alfabética

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter filter={filter} setFilter={setFilter} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list" id="todo-list">
         {todos
           .filter((todo) =>
@@ -65,6 +65,11 @@ function App() {
           )
           .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
           )
           .map((todo) => (
             <Todo
